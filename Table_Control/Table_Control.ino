6c4a36105d7b1fcff434ac73fb5cd7d3162313e2
CodeSentiel <34948535+CodeSentiel@users.noreply.github.com>
2020-06-25 15:35:27 -0700

    Update Table_Control.ino

--- Table_Control/Table_Control.ino
+++ Table_Control/Table_Control.ino
@@ -1,9 +1,68 @@
-void setup() {
-  // put your setup code here, to run once:
+String command;
 
+int gameTime;
+int gameMin;
+int gameSec;
+bool gameStatus;
+
+int currentMillis;
+int prevMillis = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  gameStatus = false;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  commadHandler();
 
+  if(command == (gameStart))
+  {
+    gameStatus = true;
+    gameTime = 180;
+  }
+  
+  if(gameStatus)
+  {
+    gameTimer();
+  }
+}
+
+void commandHandler()
+{
+  if(Serial.available() > 0)
+  {
+    command = Serial.read();
+    Serial.print("Recieved Command: ");
+    Serial.println(command);
+  }
+}
+
+void gameTimer()
+{
+  currentMillis = millis();
+
+  if((currentMillis - prevMillis) >= 1000)
+  {
+    gameTime--;
+    prevMillis = currentMillis;
+  }
+
+  gameMin = gameTime / 60;
+  gameSec = gameTime % 60;
+
+  Serial.print(gameMin);
+  Serial.print(":");
+  if(gameSec < 10)
+  {
+    Serial.print("0");
+  }
+  Serial.println(gameSec);
+
+  if(gameTime = 0)
+  {
+    gameStatus = false;
+  }
 }
