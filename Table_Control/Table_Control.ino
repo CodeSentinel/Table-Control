c99eb2e8ca50ed38be8d231de56ed2cdd8452843
CodeSentiel <34948535+CodeSentiel@users.noreply.github.com>
2020-07-02 02:26:31 -0700

    Annotations
    Added notes to the code for readibility

--- Table_Control/Table_Control.ino
+++ Table_Control/Table_Control.ino
@@ -1,16 +1,16 @@
-#define REFRESH_INTERVAL 750
+#define REFRESH_INTERVAL 750          // display refresh interval
 
-int dispMillis;
+int dispMillis;         // millis variables for display refresh
 int prevDispMillis = 0;
 
 String command;
 
-int matchTime;
+int matchTime;          // match status variables
 int matchMin;
 int matchSec;
 bool matchStatus;
 
-int diffMillis;
+int diffMillis;         // match timekeeping millis
 int matchMillis;
 int prevMatchMillis = 0;
 
@@ -20,16 +20,16 @@ void setup()
   matchStatus = false;
 }
 
-void commandHandler()
+void commandHandler()         // function to handle serial command (mainly used for serial line commands during prototyping)
 {
-  if(Serial.available() > 0)
+  if(Serial.available() > 0)          // checks for a command in the serial port and writes it to a string if available
   {
     command = Serial.read();
     Serial.print("Recieved Command: ");
     Serial.println(command);
   }
 
-  if(command == ("start match"))
+  if(command == ("start match"))          // start match command resets matchTime variable to 180 before begining the match
   {
     matchStatus = true;
     matchTime = 180;
@@ -47,34 +47,34 @@ void commandHandler()
   }
 }
 
-void matchTimer()
+void matchTimer()         // funtion that handles the timekeeping of a match
 {
   matchMillis = millis();
 
-  if((matchMillis - prevMatchMillis) > diffMillis)
+  if((matchMillis - prevMatchMillis) > diffMillis)          // determines if the match was paused and corrects for time shift
   {
     prevMatchMillis = matchMillis - diffMillis;
   }
 
-  diffMillis = matchMillis - prevMatchMillis;
+  diffMillis = matchMillis - prevMatchMillis;         // determines time difference for later referencing during match resume
 
-  if((matchMillis - prevMatchMillis) >= 1000)
+  if((matchMillis - prevMatchMillis) >= 1000)         // decriments the timer every 1 second
   {
     matchTime--;
     prevMatchMillis = matchMillis;
   }
 
-  if(matchTime = 0)
+  if(matchTime = 0)         // ends match at 0 second mark
   {
     matchStatus = false;
   }
 }
 
-void timeDisplay()
+void timeDisplay()          // function to handle the display of the match timer
 {
   dispMillis = millis();
   
-  if((dispMillis - prevDispMillis) == REFRESH_INTERVAL)
+  if((dispMillis - prevDispMillis) == REFRESH_INTERVAL)         // checks to see if the display needs to be refreshed
   {
     prevDispMillis = dispMillis;
     
@@ -83,7 +83,7 @@ void timeDisplay()
 
     Serial.print(matchMin);
     Serial.print(":");
-    if(matchSec < 10)
+    if(matchSec < 10) `       /// if the seconds are smaller than 10 it will add a 0 to account for the missing digit
     {
       Serial.print("0");
     }
@@ -91,7 +91,7 @@ void timeDisplay()
   }
 }
 
-void loop() 
+void loop()         // main loop for calling other functions
 {
   commandHandler();
   
