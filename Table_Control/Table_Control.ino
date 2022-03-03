abdbf50c5f284c2e007d01ec0670c377188c00d7
Gabriel Contreras <10gabrielc@gmail.com>
2022-03-03 15:40:16 -0800

    Added more comments to main file
    Added heading comments for the program, global variables, and some functions to help guide the code development

--- Table_Control/Table_Control.ino
+++ Table_Control/Table_Control.ino
@@ -1,29 +1,51 @@
+/*
+ *  MAIN PROGRAM FILE FOR THE INTERACTIVE SOCCER TABLE PROJECT, "TABLE TEAM",
+ *  FOR CAL POLY POMONA ROBOTICS CLUB (2021-2022)
+ * 
+ *  THE MAIN GOALS OF THIS PROGRAM ARE AS FOLLOWS:
+ *  --recognize competing teams using RFID cards and scanner
+ *  --keep team information stored physically (SD Card)
+ *  --detect goals or other important game events
+ *  --display match time and each team's score, for both players and the audience
+ *  --display immersive effects for game events using an individually addressable LED matrix
+ */
+
+// ALL LIBRARIES REQUIRED FOR THE PROGRAM
 #include "libs/FastLED/FastLED.h"         // version 3.3.3
 
+// ALL CONSTANTS USED THROUGHOUT THE CODE
 #define REFRESH_INTERVAL 500          // time refresh interval in milliseconds
 #define IR_SENSOR 2                   // connect ir sensor to pin 2
 
-int dispMillis;         // millis variables for display refresh
-int prevDispMillis = 0;
+// ALL VARIABLES NEEDED FOR TIMING USING millis()
+int diffMillis  = 100;         // match timekeeping variables
+int matchMillis;
+int prevMatchMillis = 0;
+const unsigned int scoreboardMillisDelay = 500;   // delay between scoreboard refreshes
+unsigned int prevScoreboardMillis = 0             // storage for last time scoreboard refreshed
 
-String command;
+// ALL VARIABLES NEEDED FOR GAME MECHANICS AND TIMING
+int matchTime;                // match time in seconds
+byte scoreTeam1 = 0;          // score counter for team 1
+byte scoreTeam2 = 0;          // score counter for team 2
+bool matchStatus = false;     // flag for if a game is active
+bool goalScored = false;      // flag for if a goal has been scored
 
-int matchTime;          // match status variables
+// ALL VARIABLES NEEDED FOR TIME AND GOAL DISPLAY SYSTEM
 int matchMin;
 int matchSec;
 int matchSecTens;
 int matchSecOnes;
-bool matchStatus = false;
-bool goalScored = false;          // code test parameter
 
-int diffMillis  = 100;         // match timekeeping variables
-int matchMillis;
-int prevMatchMillis = 0;
-
+// ALL VARIABLES NEEDED FOR DISPLAYING ANIMATIONS
 bool startAni = false;
 bool endAni = false;
 bool goalAni = false;
 
+// ALL VARIABLES USED FOR DEBUGGING
+String command;
+
+// REQUIRED SETUP FUNCTION FOR THE PROGRAM
 void setup() 
 {
   Serial.begin(9600);
@@ -89,7 +111,7 @@ void matchTimer()         // funtion that handles the timekeeping of a match
     prevMatchMillis = matchMillis;
   }
 
-  if(matchTime == 0)         // ends match at 0 second mark
+  if(matchTime <= 0)         // ends match at 0 second mark
   {
     matchStatus = false;
     endAni = true;
@@ -115,8 +137,6 @@ void timeDisplay()          // function to handle the display of the match timer
     Serial.println(matchSecOnes);
   }
 }
-
-
 
 void goalDetection()
 {
@@ -149,6 +169,24 @@ void animationHandler()
   }
 }
 
+
+/*
+ * MAIN PROGRAM LOOP FOR THE PROGRAM BELOW. GENERAL CODE SHOULD FLOW AS FOLLOWS:
+ * --while game is not in progress, check RFID scanner to scan for two distinct teams
+ * --upon each time "signing in", load their team data for the match (color, team name, goals)
+ * --after two teams signed in, reset all match variables and wait for match start (button press)
+ * --ONCE A MATCH HAS STARTED:
+ * ----decrement match time each second
+ * ----check goal sensors for if a goal occured. if so, pause match and increment score
+ * ----play any animations that correspond with match events
+ * ----refresh the scoreboard to reflect any time change or score change
+ * ----check for any external button presses (timeouts, necessary pauses, errors)
+ * ----check for if match has ended using match timer
+ * --ONCE A MATCH HAS ENDED:
+ * ----store match statistics to physical media (SD Card)
+ * ----play any animations that correspond with end of match
+ * ----prepare RFID scanner for next check-in
+ */
 void loop()         // main loop for calling other functions
 {
   commandHandler();
