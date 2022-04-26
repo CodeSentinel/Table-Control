d9412d719e761ad40e47f5d88db4e932f61b1b1c
Gabriel Contreras <10gabrielc@gmail.com>
2022-04-25 20:25:49 -0700

    Button IO in Main OS
    TableOS now has 4 button IO implementation. The buttons are debounced using flags and timing, and there is a whole function dedicated to determining the button state. Pin instantiation, global variables, gameInput, and main function modified

--- Table_Control/Table_Control.ino
+++ Table_Control/Table_Control.ino
@@ -19,9 +19,9 @@
 #define MATCH_LENGTH 180                    // time of one match
 #define REFRESH_INTERVAL 100              // time refresh interval in milliseconds
 #define LEFT_BUTTON 3
-#define RIGHT_BUTTON 4
+#define RIGHT_BUTTON 6
 #define PLAY_PAUSE 5
-#define START_MATCH 6
+#define START_MATCH 4
 
 // ALL VARIABLES NEEDED FOR TIMING USING millis()
 int diffMillis  = 100;                            // match timekeeping variables
@@ -29,6 +29,8 @@ int matchMillis;
 int prevMatchMillis = 0;
 const unsigned int scoreboardMillisDelay = 500;   // delay between scoreboard refreshes
 unsigned int prevScoreboardMillis = 0;             // storage for last time scoreboard refreshed
+const unsigned int ioMillisDelay = 50;            //delay between IO checks
+unsigned int prevIoMillis = 0;                    //storage for last time IO was checked
 
 // ALL VARIABLES NEEDED FOR GAME MECHANICS AND TIMING
 int matchTime = 0;                                    // match time in seconds
@@ -37,6 +39,10 @@ byte scoreTeam2 = 0;                                  // score counter for team 
 bool matchStatus = false;                             // flag for if a game is active
 bool goalScored = false;                              // flag for if a goal has been scored
 String dataOut;                                       // outputs to serial for computer interface
+bool btnLeftGoalPressed = false;                      //flags for all the pressable buttons
+bool btnRightGoalPressed = false;
+bool btnStartGamePressed = false;
+bool btnPauseGamePressed = false;
 
 // ALL VARIABLES NEEDED FOR TIME AND GOAL DISPLAY SYSTEM
 int matchMin = 0;
@@ -54,10 +60,10 @@ void setup()
 {
   Serial.begin(9600);
   //Serial.println("System Start");
-  pinMode(LEFT_BUTTON, INPUT);               // set pins as input
-  pinMode(RIGHT_BUTTON, INPUT);
-  pinMode(PLAY_PAUSE, INPUT);
-  pinMode(START_MATCH, INPUT);
+  pinMode(LEFT_BUTTON, INPUT_PULLUP);               // set pins as input
+  pinMode(RIGHT_BUTTON, INPUT_PULLUP);
+  pinMode(PLAY_PAUSE, INPUT_PULLUP);
+  pinMode(START_MATCH, INPUT_PULLUP);
 }
 
 void commandHandler()                       // function to handle serial command (mainly used for serial line commands during prototyping)
@@ -167,20 +173,23 @@ void timeDisplay()                       // function to handle the display of th
 
 void gameInput()
 {
-  if(digitalRead(LEFT_BUTTON))
+  if(btnLeftGoalPressed)
   {
+    btnLeftGoalPressed = false;
     matchStatus = false;
     scoreTeam1 = scoreTeam1 + 1;
   }
 
-  if(digitalRead(RIGHT_BUTTON))
+  if(btnRightGoalPressed)
   {
+    btnRightGoalPressed = false;
     matchStatus = false;
     scoreTeam2 = scoreTeam2 + 1;
   }
 
-  if(digitalRead(PLAY_PAUSE))
+  if(btnPauseGamePressed)
   {
+    btnPauseGamePressed = false;
     if(matchStatus)
     {
       matchStatus = false;
@@ -191,14 +200,78 @@ void gameInput()
     }
   }
 
-  if(digitalRead(START_MATCH))
+  if(btnStartGamePressed)
   {
+    btnStartGamePressed = false;
     if(matchStatus = false)
     {
       matchTime = MATCH_LENGTH;
       scoreTeam1 = 0;
       scoreTeam2 = 0;
     }
+  }
+}
+
+void CheckButtonStatus()
+{
+  /*
+   * To check for changes to a push button, while also debouncing the signal, we need to
+   * check the current state of the button, as well as the last state of the button. If
+   * the current button state is different from the previous, then the button has either
+   * been pressed or released. To prevent button bounce, the function should also only
+   * occur once every 20-30ms or so. The flag for buttons is cleared when a button is
+   * released, or can be cleared by a function that uses the button (to prevent multiple
+   * functions that use the same button from occuring)
+  */
+  //CHECK FOR TIMING
+  unsigned int currentTime = millis();
+  if((currentTime - prevIoMillis) >= ioMillisDelay)
+  {
+    //READ ALL BUTTON STATES
+    bool leftGoalState = digitalRead(btnLeftGoal);
+    bool rightGoalState = digitalRead(btnRightGoal);
+    bool pauseGameState = digitalRead(btnPauseGame);
+    bool startGameState = digitalRead(btnStartGame);
+  
+    //CHECK FOR CHANGE TO FIRST BUTTON
+    if(leftGoalState == LOW && btnLeftGoalPressed == false)
+    {
+      //BUTTON HAS BEEN PRESSED
+      btnLeftGoalPressed = true;
+    }
+    else if(leftGoalState == HIGH && btnLeftGoalPressed == true)
+    {
+      //BUTTON HAS BEEN RELEASED
+      btnLeftGoalPressed = false;
+    }
+    else
+    {
+      //BUTTON IS BEING HELD DOWN OR HASN'T BEEN TOUCHED
+      //DO NOTHING
+    }
+  
+    //CHECK FOR SECOND BUTTON CHANGE
+    if(rightGoalState == LOW && btnRightGoalPressed == false)
+      btnRightGoalPressed = true;
+    else if(rightGoalState == HIGH && btnRightGoalPressed == true)
+      btnRightGoalPressed = false;
+    else{}
+  
+    //CHECK FOR THIRD BUTTON CHANGE
+    if(pauseGameState == LOW && btnPauseGamePressed == false)
+      btnPauseGamePressed = true;
+    else if(pauseGameState == HIGH && btnPauseGamePressed == true)
+      btnPauseGamePressed = false;
+    else{}
+  
+    //CHECK FOR FOURTH BUTTON CHANGE
+    if(startGameState == LOW && btnStartGamePressed == false)
+      btnStartGamePressed = true;
+    else if(startGameState == HIGH && btnStartGamePressed == true)
+      btnStartGamePressed = false;
+    else{}
+
+    prevIoMillis = currentTime;
   }
 }
 
@@ -223,7 +296,9 @@ void gameInput()
 void loop()                 // main loop for calling other functions
 {
   commandHandler();
-
+  
+  CheckButtonStatus();
+  
   gameInput();
   
   if(matchStatus)          // timer runs only when the match status is true
